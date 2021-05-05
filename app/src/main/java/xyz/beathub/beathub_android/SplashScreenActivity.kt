57f2efdcecd76f3347ab4c9df034527d512bdc8f
vanjavk <developer@vanjavk.me>
2021-05-05 16:25:26 +0200

    pokretanje inita

---
+++ app/src/main/java/xyz/beathub/beathub_android/SplashScreenActivity.kt
@@ -0,0 +1,52 @@
+package xyz.beathub.beathub_android
+
+import android.content.ContentUris
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import me.vanjavk.recenzo.framework.applyAnimation
+import me.vanjavk.recenzo.framework.getBooleanPreference
+import me.vanjavk.recenzo.framework.isOnline
+import me.vanjavk.recenzo.framework.startActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+private const val DELAY : Long = 3000
+const val DATA_IMPORTED  = "me.vanjavk.recenzo.data_already_imported"
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        startAnimations()
+        redirect()
+    }
+
+    private fun startAnimations() {
+        ivSplash.applyAnimation(R.anim.rotate)
+        tvSplash.applyAnimation(R.anim.enlarge)
+    }
+
+    private fun redirect() {
+
+        if (getBooleanPreference(DATA_IMPORTED)) {
+            println("Data already imported!")
+            Handler(Looper.getMainLooper()).postDelayed(
+                {startActivity<HostActivity>()},
+                DELAY
+            )
+        } else {
+        if (isOnline()) {
+            // start service //refresh database
+
+            Intent(this, RecenzoService::class.java).apply {
+                RecenzoService.enqueueWork(this@SplashScreenActivity, this)
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.please_connect_to_the_internet), Toast.LENGTH_SHORT).show()
+            finish()
+        }
+       }
+    }
+}
