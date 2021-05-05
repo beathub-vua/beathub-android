9fd5c152435f1949bf47f50f78bf867ab8ca2de6
vanjavk <developer@vanjavk.me>
2021-05-05 16:10:48 +0200

    init

---
+++ app/src/main/java/xyz/beathub/beathub_android/BeathubReceiver.kt
@@ -0,0 +1,15 @@
+package me.vanjavk.recenzo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import xyz.beathub.beathub_android.HostActivity
+
+
+class BeathubReceiver : BroadcastReceiver() { // BroadcastReceiver aint no Context
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        context.startActivity<HostActivity>()
+    }
+}
